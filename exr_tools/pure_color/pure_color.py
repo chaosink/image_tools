c54738e4a682ddb004b87a5277c8943ac061b7a9
linhsu0723 <linhsu0723@qq.com>
2020-03-25 19:38:02 +0800

    pure_color: 1/3/4 channels, all can be given a specified value

--- exr_tools/pure_color/pure_color.py
+++ exr_tools/pure_color/pure_color.py
@@ -4,17 +4,24 @@ import pyexr
 import sys
 import numpy as np
 
-if len(sys.argv) != 4:
-	print("pure_color: Generate pure color EXRs(1 and 3 channels) with specific value and size.")
-	print("Usage: pure_color.py width height value")
+if len(sys.argv) != 4 and len(sys.argv) != 6 and len(sys.argv) != 7:
+	print("pure_color: Generate pure color EXRs(1/3/4 channels) with specific value and size.")
+	print("Usage: pure_color.py width height channel_1 [channel_2 channel_3 [channel_4]]")
 	exit()
 
 width = int(sys.argv[1])
 height = int(sys.argv[2])
-value = float(sys.argv[3])
+n_channel = len(sys.argv) - 3
+value = np.zeros(n_channel)
+exr_name = "%dx%d" % (width, height)
+for i in range(n_channel):
+	value[i] = float(sys.argv[i + 3])
+	exr_name += "_%.2f" % value[i]
 
-rgb = np.ones((height, width, 3)) * value
-y = np.ones((height, width, 1)) * value
+img = np.ones((height, width, n_channel)) * value
 
-pyexr.write('%d_%d_%.2f_3.exr' % (width, height, value), rgb)
-pyexr.write('%d_%d_%.2f_1.exr' % (width, height, value), y, channel_names=['Y'])
+exr_name += ".exr"
+if n_channel == 1:
+	pyexr.write(exr_name, img, channel_names=['Y'])
+else:
+	pyexr.write(exr_name, img)
--- exr_tools/pure_color/test.sh
+++ exr_tools/pure_color/test.sh
@@ -2,3 +2,8 @@
 
 ./pure_color.py 512 512 0
 ./pure_color.py 512 512 1
+./pure_color.py 512 512 0 0 0
+./pure_color.py 512 512 1 0 0
+./pure_color.py 512 512 0 1 0
+./pure_color.py 512 512 0 0 1
+./pure_color.py 512 512 1 1 1 1
