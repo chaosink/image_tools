c2f94545f8025adac59ee88c72d24b6ead2540ce
linhsu0723 <linhsu0723@qq.com>
2020-04-01 11:42:17 +0800

    exror: MAE RMAE RootMSE PSNR

--- exr_tools/exror/exror.py
+++ exr_tools/exror/exror.py
@@ -12,17 +12,50 @@ module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 exrtime = module.exrtime
 
+# Sum of Absolute Error
+def SAE(img_refer, img_noisy):
+	d = img_noisy - img_refer
+	d = np.abs(d)
+	sae = d.sum()
+	return sae
+
+# Sum of Squared Error
 def SSE(img_refer, img_noisy):
 	d = img_noisy - img_refer
 	d *= d
 	sse = d.sum()
 	return sse
 
+# Mean Absolute Error
+def MAE(img_refer, img_noisy):
+	sae = SAE(img_refer, img_noisy)
+	mae = sae / img_noisy.size
+	return mae
+
+# Mean Squared Error
 def MSE(img_refer, img_noisy):
+	return mean_squared_error(img_refer, img_noisy)
 	sse = SSE(img_refer, img_noisy)
 	mse = sse / img_noisy.size
 	return mse
 
+# Root Mean Squared Error
+def RootMSE(img_refer, img_noisy):
+	mse = MSE(img_refer, img_noisy)
+	root_mse = np.sqrt(mse)
+	return root_mse
+
+# Relative Mean Absolute Error
+def RMAE(img_refer, img_noisy):
+	d = img_noisy - img_refer
+	d = np.abs(d)
+	ref = img_refer + 0.01
+	d = d / ref
+	rmae = d.sum()
+	rmae = rmae / img_noisy.size
+	return rmae
+
+# Relative Mean Squared Error
 def RMSE(img_refer, img_noisy):
 	d = img_noisy - img_refer
 	d *= d
@@ -32,12 +65,19 @@ def RMSE(img_refer, img_noisy):
 	rmse = rmse / img_noisy.size
 	return rmse
 
-# def CorrectGamma(Mat &img)
-# 	Mat m
-# 	pow(img, 1 / 2.2, m)
-# 	m *= 255
-# 	m.convertTo(m, CV_8U)
-# 	return m
+def SSIM(img_refer, img_noisy):
+	return structural_similarity(img_refer, img_noisy, multichannel=True, gaussian_weights=True)
+
+def Tonemap(hdr, gamma=2.2):
+	ldr = np.clip(hdr, 0, 1)
+	ldr = np.power(hdr, gamma)
+	ldr = np.round(hdr)
+	return ldr
+
+def PSNR(img_refer, img_noisy):
+	img_refer_ldr = Tonemap(img_refer)
+	img_noisy_ldr = Tonemap(img_noisy)
+	return peak_signal_noise_ratio(img_refer_ldr, img_noisy_ldr, data_range=255)
 
 if len(sys.argv) < 3:
 	print("exror: Evaluate image in MSE, rMSE and SSIM.")
@@ -58,14 +98,15 @@ for i in range(2, len(sys.argv)):
 		print("	%s:" % img_noisy_name, img_noisy.shape)
 		exit(1)
 
-	ssim = structural_similarity(img_refer, img_noisy, multichannel=True, gaussian_weights=True)
-	# psnr = peak_signal_noise_ratio(img_refer, img_noisy, 255)
-	mse = mean_squared_error(img_refer, img_noisy)
 	time = exrtime(img_noisy_name)
 	print(img_noisy_name)
 	if time:
-		print("Time:", time)
-	print("MSE :", mse)
-	print("RMSE:", RMSE(img_refer, img_noisy))
-	print("SSIM:", ssim)
+		print("Time :", time)
+	print("MAE ↓:", MAE(img_refer, img_noisy))
+	print("MSE ↓:", MSE(img_refer, img_noisy))
+	# print("RootMSE:", RootMSE(img_refer, img_noisy))
+	print("RMAE↓:", RMAE(img_refer, img_noisy))
+	print("RMSE↓:", RMSE(img_refer, img_noisy))
+	print("SSIM↑:", SSIM(img_refer, img_noisy))
+	print("PSNR↑:", PSNR(img_refer, img_noisy))
 	print()
